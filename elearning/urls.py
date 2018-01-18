87acce57e2387eb0818b847e8fd4ba8ef37359d0
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2018-01-18 10:51:18 -0500

    Tab update urls.py

--- elearning/urls.py
+++ elearning/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 
 from rest_framework import routers
 
-from courses.views import (course_add, course_detail, course_list, do_section, do_test, show_results, SectionViewSet)
-
-from students.views import student_detail
+from courses.views import (course_add, course_detail, course_list, do_section,
+                           do_test, show_results, SectionViewSet)
+from students.views import student_detail, UserViewSet
 from api.views import UserViewSet
 
 router = routers.DefaultRouter()
@@ -30,17 +30,12 @@ router.register(r'sections', SectionViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-
     url('^', include('django.contrib.auth.urls', namespace='auth')),
-
     url(r'^course_detail/(?P<pk>\d+)/$', course_detail, 
     	name='course_detail'),
-    
     url(r'^section/(?P<section_id>\d+)/$', do_section, name='do_section'),
     url(r'^section/(?P<section_id>\d+)/test/$', do_test, name='do_test'),
-
     url(r'^section/(?P<section_id>\d+)/results/$', show_results, name='show_results'),
-
     url(r'^course_add/$', course_add, name='course_add'),
     url(r'^student_detail/$', student_detail, 
         name='student_detail'),
